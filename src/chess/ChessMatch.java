130279f559db27b18748beff702c758554ae8c62
Jeff-Aug <dentyn_tuey@outlook.com>
2020-12-01 00:27:37 -0300

    Colorindo os tracinhos

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,6 +34,15 @@ public class ChessMatch {
 	}
 	
 	
+	public boolean[][] possibleMoves(ChessPosition sourcePosition){
+		
+		Position position = sourcePosition.toPosition();
+		validateSourcePosition(position);
+		return board.piece(position).possibleMoves();
+	}
+	
+	
+	
 	//fica respossavel por mover as peças no tabuleiro
 	public ChessPiece performChessMove(ChessPosition sourcePosition,ChessPosition targetPosition) {
 		
